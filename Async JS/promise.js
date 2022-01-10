ec9d0f58bf88c5bfa4c2bc6f01a92bbfbce9235f
kchen <kchen@a10networks.com>
2022-01-10 21:51:30 +0800

    2 ways to consume promises

--- Async JS/promise.js
+++ Async JS/promise.js
@@ -3,7 +3,7 @@ function doSomethingAsyc(duration) {
     console.log("......");
 
     setTimeout(
-      () => resolve("Finished async tasks after " + duration / 1000),
+      () => resolve("Finished async tasks after " + duration+ " miliseconds"),
       duration
     );
   });
@@ -16,5 +16,15 @@ const doSomething = async () => {
 };
 
 console.log("before doSomething()");
+
+// First way to consume promise
+// doSomethingAsyc(3000).then((msg)=>{
+//   console.log(msg);
+// }).catch((err)=>{
+//   console.log(err)
+// })
+
+// Second way to consume promise
+// Consume it inside an async function
 doSomething();
 console.log("after doSomething()");
