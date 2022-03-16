c6be717b0c79ef9e0d721c4c2fa1705c73c468f1
kchen <kchen@a10networks.com>
2022-03-16 10:32:36 +0800

    [Prototype] update the example of using protoype system in JS

--- OOP/prototype based/basicUsage.js
+++ OOP/prototype based/basicUsage.js
@@ -1,8 +1,17 @@
+/*
+  Howis an object created?
+  When an object is created via "new Foo(args...)" keyword,
+    1. An generic object is created as "this"
+    2. Set its __proto__ property to Foo.prototype 
+    3. Initialize "this" in the constructor Foo
+*/
+
 function Employee(dept = "general") {
   this.name = "";
   this.dept = dept;
 }
 Employee.prototype.specialty = "None";
+Employee.prototype.boss = "Kevin";
 
 function Manager() {
   Employee.call(this);
@@ -10,46 +19,33 @@ function Manager() {
 }
 
 Manager.prototype = Object.create(Employee.prototype);
-
-/*
-    When creating an Manager object,
-    1. Create an generic object as "this"
-    2. Set its __proto__ property to Manager.prototype 
-    3. Initialize "this" in the constructors Manager and Employee
-*/
+Manager.prototype.specialty = "manage"
 m = new Manager();
-console.log(`m.specialty is ${m.specialty}`);
-console.log(`m.dept is ${m.dept}`);
-
-// Not neccesary I think, but used in MDN.
-// Manager.prototype.constructor = Manager;
 
 function WorkerBee() {
   Employee.call(this);
   this.projects = [];
 }
+
 WorkerBee.prototype = Object.create(Employee.prototype);
-// Not neccesary I think, but used in MDN.
-// WorkerBee.prototype.constructor = WorkerBee;
+WorkerBee.prototype.workHours = 8
 
 function SalesPerson() {
   WorkerBee.call(this);
   this.dept = "sales";
   this.quota = 100;
 }
+
 SalesPerson.prototype = Object.create(WorkerBee.prototype);
-// Not neccesary I think, but used in MDN.
-// SalesPerson.prototype.constructor = SalesPerson;
+SalesPerson.prototype.specialty = "sell"
 
 function Engineer() {
   WorkerBee.call(this);
   this.dept = "engineering";
   this.machine = "";
 }
+
 Engineer.prototype = Object.create(WorkerBee.prototype);
 Engineer.prototype.specialty = "code";
 
 eng = new Engineer();
-
-// the prototype chain lookup stops at Engineer.prototype
-console.log(`eng.specialty is ${eng.specialty}`);
